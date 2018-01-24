f87d0abb8c9e5988b184d7d4d75a258092cfb233
Frank Liao <recs7168@gmail.com>
2018-01-24 01:25:53 -0500

    Date.today -> Date.current

--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -7,7 +7,7 @@ class ChoreTest < ActiveSupport::TestCase
   should belong_to(:task)
   should allow_value(1.day.from_now.to_date).for(:due_on)
   should allow_value(1.day.ago.to_date).for(:due_on)
-  should allow_value(Date.today).for(:due_on)
+  should allow_value(Date.current).for(:due_on)
   should_not allow_value("bad").for(:due_on)
   should_not allow_value(3.14159).for(:due_on)
 
